d202e288a26bba7932db699b25d81e61b6197d6b
Julio Martinez <juliojwfuh@gmail.com>
2020-06-26 22:51:17 -0500

    ESTILOS CSS Y PARTE DE VALIDAR DATOS

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
+$errores='';
+$enviado='';
+
+if (isset($_POST['submit'])){
+    $nombre=$_POST['nombre'];
+    $correo=$_POST['correo'];
+    $mensage=$_POST['mensage'];
+
+    if (!empty($nombre)) {
+        $nombre= trim($nombre);
+        $nombre= filter_var($nombre, FILTER_SANITIZE_STRING);
+    }else {
+        $errores .= 'Por favor ingresa un nombre <br/>';
+    }
+    if(!empty($correo)){
+        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            $errores .= 'Por favor ingresa un correo valido <br />';
+        }
+    } else {
+        $errores .= 'Por favor ingresa un correo <br />';
+    }   
+
+}
 require  'index.view.php';
 ?>
--- index.view.php
+++ index.view.php
@@ -4,22 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario contacto</title>
+    <link rel="stylesheet" href="estilos.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" type='text/css'>
-    <link rel="stylesheet" href="estilo.css">
+    
      
 </head>
 <body>
     <div class="wrap">
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <input type="text" class="form-control"  id="nombre"name="nombre" placeholder="Nombre:" value="">
-
-        <input type="text" class="form-control" id="correo" name="correo" placeholder="Correo:" value="">
-
-        <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"></textarea>
-        <input type="submit" name="submit" class="btn btn-primary" value="Enviar Correo">
-
-    </form>
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre:" value="">
+            <input type="text" class="form-control" id="correo" name="correo" placeholder="Correo:" value="">
+            <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"></textarea>
+            <?php if (!empty($errores)): ?>
+                <div class="alert error">
+                    <?php echo $errores; ?>
+                </div>
+            <?php elseif($enviado): ?>
+                <div class="alert success">
+                    <p>Eviado Correctamente</p>
+                </div>
+            <?php endif ?>
+            <input type="submit" name="submit" class="btn btn-primary" value="Enviar correo">
+        </form>
     </div>
-    
 </body>
+</html>
 </html>
