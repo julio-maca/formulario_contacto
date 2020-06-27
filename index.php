75dad6b425f879dc3ce2a9159e80f5224ba40a1a
Julio Martinez <juliojwfuh@gmail.com>
2020-06-26 23:03:16 -0500

    FIN de la practica

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $enviado='';
 if (isset($_POST['submit'])){
     $nombre=$_POST['nombre'];
     $correo=$_POST['correo'];
-    $mensage=$_POST['mensage'];
+    $mensaje=$_POST['mensaje'];
 
     if (!empty($nombre)) {
         $nombre= trim($nombre);
@@ -22,6 +22,26 @@ if (isset($_POST['submit'])){
     } else {
         $errores .= 'Por favor ingresa un correo <br />';
     }   
+
+    
+    if(!empty($mensaje)){
+        $mensaje = htmlspecialchars($mensaje);
+        $mensaje = trim($mensaje);
+        $mensaje = stripslashes($mensaje);
+        
+    } else {
+        $errores .= 'Por favor ingresa el mensaje <br />';
+    }
+
+    if(!$errores){
+        $enviar_a = 'Sleepypancake.com';
+        $asunto = 'Correo enviado desde mi pagina.com';
+        $mensaje_separado = "De: $nombre \n";
+        $mensaje_separado .= "Correo: $correo \n";
+        $mensaje_separado .= "Mensaje: " . $mensaje;
+
+        $enviado = 'true';
+    }
 
 }
 require  'index.view.php';
